5b3746f83133a089c460e44f7aa83c7b9c50181e
alyonaVet <a.v.alenka2015@gmail.com>
2024-05-30 13:20:53 +0600

    refactor component Ingredient;

--- Homework56/src/components/Ingredient/Ingredient.tsx
+++ Homework56/src/components/Ingredient/Ingredient.tsx
@@ -6,11 +6,12 @@ interface Props {
   className: string;
   name: string;
   image: string;
+  onClick: React.MouseEventHandler;
 }
 
-const Ingredient: React.FC<Props> = ({className, type, name, image}) => {
+const Ingredient: React.FC<Props> = ({className, type, name, image, onClick}) => {
   return (
-    <button type={type} className={className}>
+    <button type={type} className={className} onClick={onClick}>
       <img src={image} alt={name} className="img"/>
       <span className="ingredientName">{name}</span>
     </button>
